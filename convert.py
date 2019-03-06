18586b545b92187ae7af10b0b23564a7c3f6d3b9
jrdnbradford <36420801+jrdnbradford@users.noreply.github.com>
2019-03-06 14:23:10 -0500

    Expand Air-Date.

--- convert.py
+++ convert.py
@@ -30,28 +30,39 @@ def json_to_csv():
 
 def json_to_xml():
     """Converts original JSON to XML"""
-    
+    import calendar
+
+
+    months = {month: num for num, month in enumerate(calendar.month_name)}
+
     with open("XML/episodes.xml", "w") as new_xml:
         with open("JSON/episodes.json", "r") as json_file:
             data = json.load(json_file)
-            new_xml.write('<?xml version="1.0" encoding="UTF-8"?>\n\n')
-            new_xml.write(f"<The-Office>\n\n") #Begin XML
+            new_xml.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+            new_xml.write(f"<The-Office>\n") #Begin XML
             for season in data:
                 season_tag = season.replace(" ", "-")
-                new_xml.write(f"\t<{season_tag}>\n\n") #Begin Season
+                new_xml.write(f"\t<{season_tag}>\n") #Begin Season
                 for episode in data[season]:
                     episode_tag = episode.replace(" ", "-") 
                     new_xml.write(f"\t\t<{episode_tag}>\n") #Begin Episode
                     for key in data[season][episode]:
-                        if isinstance(data[season][episode][key], list): #For Writers
-                            key_tag = key.replace("(s)", "s")
-                            new_xml.write(f"\t\t\t<{key_tag}>\n") #Begin Writers
-                            for i, writer in enumerate(data[season][episode][key]):
+                        datum = data[season][episode][key]
+                        tag = key.replace(" ", "-").replace("(s)", "s")
+                        if isinstance(datum, list): #For Writers
+                            new_xml.write(f"\t\t\t<{tag}>\n") #Begin Writers
+                            for i, writer in enumerate(datum):
                                 new_xml.write(f"\t\t\t\t<Writer-{i+1}>{writer}</Writer-{i+1}>\n")
-                            new_xml.write(f"\t\t\t</{key_tag}>\n") #End Writers
+                            new_xml.write(f"\t\t\t</{tag}>\n") #End Writers
+                        elif key == "Air Date":
+                            new_xml.write(f"\t\t\t<{tag}>\n") #Begin Air-Date
+                            month, day, year = datum.replace(",", "").split(" ")
+                            new_xml.write(f"\t\t\t\t<Year>{year}</Year>\n")
+                            new_xml.write(f"\t\t\t\t<Month>{months[month]}</Month>\n")
+                            new_xml.write(f"\t\t\t\t<Day>{day}</Day>\n")
+                            new_xml.write(f"\t\t\t</{tag}>\n") #End Air-Date
                         else:
-                            key_tag = key.replace(" ", "-")
-                            new_xml.write(f"\t\t\t<{key_tag}>{data[season][episode][key]}</{key_tag}>\n") 
-                    new_xml.write(f"\t\t</{episode_tag}>\n\n") #End Episode     
-                new_xml.write(f"\t</{season_tag}>\n\n") #End Season
+                            new_xml.write(f"\t\t\t<{tag}>{datum}</{tag}>\n") 
+                    new_xml.write(f"\t\t</{episode_tag}>\n") #End Episode     
+                new_xml.write(f"\t</{season_tag}>\n") #End Season
             new_xml.write(f"</The-Office>") #End XML
